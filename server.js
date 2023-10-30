c839a108f5a790510e8893f751579909f6d59fdb
Vipun Sanjana Jayathunga <92296561+vipunsanjana@users.noreply.github.com>
2023-10-31 02:13:47 +0530

    Updated now server.js

--- server.js
+++ server.js
@@ -6,39 +6,34 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import cors from 'cors'
-import path from 'path'
-import {fileUrlToPath} from 'url';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 connectDB();
 
-const __filename = fileUrlToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
+
 app.use('/api/v1/auth', authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'));
-})
-
-app.get('/', (req, res) => {
-    res.send({
-        message: "Welcome e-commerce app", // Fixed the message
-    });
+// Define a catch-all route to serve your React app
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    // Change ${process.env.DEV_MODE} to a static message or use an appropriate environment variable
     console.log(`Node server running on port ${PORT}`.bgGreen.white);
 });
